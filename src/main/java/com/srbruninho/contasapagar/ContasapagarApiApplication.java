9c46bd45d1fa37e3365180fb3aafca0c1bcbde92
Bruno Silva <bos.bruno.oliveira55@gmail.com>
2024-05-30 19:14:20 -0300

    1 Commit - Project Setup

---
+++ src/main/java/com/srbruninho/contasapagar/ContasapagarApiApplication.java
@@ -0,0 +1,13 @@
+package com.srbruninho.contasapagar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContasapagarApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContasapagarApiApplication.class, args);
+	}
+
+}
